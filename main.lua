423c90b006e86554b3ae6a3b097052f5dca8411a
Josie Thompson <josiahst@gmail.com>
2019-10-11 18:31:35 -0700

    letters move away from mouse when linke is clicked

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
-
 Fragment = {}
 function Fragment:new(ch)
     local fragment = {}
     setmetatable(fragment, self)
     self.__index = self
     fragment.char = ch
-    fragment.pos = {x=0, y=0}
+    fragment.center = {x=0, y=0}
+    fragment.ul = {x=0, y=0}
+    fragment.vel = {x=0, y=0}
+    fragment.q = 1  -- "charge" for calculating acceleration
     return fragment
 end
 
@@ -13,13 +15,16 @@ link = {
     text = "Utopia",
     letters = {},
     color = {0, 0, 1, 1}, -- blue to look like a hyperlink
-    broken = false
+    is_broken = false
 }
 for i = 1, #link.text do
     local c = link.text:sub(i, i)
     local f = Fragment:new(c)
     link.letters[i] = f
 end
+
+mq = 5 -- mouse "charge" for calculating acceleration
+ke = 1 -- "Coulomb's" constant
 
 function love.load()
     -- set background to be white
@@ -52,23 +57,38 @@ function love.load()
     -- assign a position to each letter
     local x = link.x
     for i, l in ipairs(link.letters) do
-        l.pos.x = x
+        local w = link.font:getWidth(l.char)
+        local h = link.font:getHeight(l.char)
+
+        -- set the center and upper-left corner x positions
+        l.ul.x = x
+        l.center.x = x + w/2
 
         -- increment the letter x-position
-        x = x + buffer + link.font:getWidth(l.char)
+        x = x + buffer + w
 
         -- find height between top of line and top of letter
-        local gap = link.height-link.font:getHeight(l.char)
-        l.pos.y = link.y + gap
+        local gap = link.height-h
+        l.ul.y = link.y + gap
+        l.center.y = l.ul.y + h/2
     end
 end
 
 function love.draw()
     link.draw()
+
+    if link.is_broken then
+        for _, l in ipairs(link.letters) do
+            l.ul.x = l.ul.x + l.vel.x
+            l.ul.y = l.ul.y + l.vel.y
+            l.center.x = l.center.x + l.vel.x
+            l.center.y = l.center.y + l.vel.y
+        end
+    end
 end
 
 function love.mousepressed(x, y, button, istouch)
-    if button == 1 and link.hovering() and not link.broken then
+    if button == 1 and link.hovering() and not link.is_broken then
         link.toggle_break()
     end
 end
@@ -85,12 +105,12 @@ function link.draw()
     -- draw text
     love.graphics.setColor(link.color)
     for i, l in ipairs(link.letters) do
-        love.graphics.print(l.char, l.pos.x, l.pos.y)
+        love.graphics.print(l.char, l.ul.x, l.ul.y)
     end
 
     -- create underline for text to look like hyperlink
     -- specifically when mouse hovers over text
-    if link.hovering() and not link.broken then
+    if link.hovering() and not link.is_broken then
         love.graphics.setLineWidth(4)
         love.graphics.line(link.x, link.y+link.height,
                            link.x+link.width, link.y+link.height)
@@ -101,14 +121,62 @@ function link.draw()
         -- change cursor from hand to arrow when cursor leaves text area
         love.mouse.setCursor(love.mouse.getSystemCursor("arrow"))
     end
+
+    -- move the characters away from the cursor if the link is broken
+    if link.is_broken then
+        for i, l in ipairs(link.letters) do
+
+            -- difference vector between mouse click and character
+            local dv = {
+                x = love.mouse.getX() - l.center.x,
+                y = love.mouse.getY() - l.center.y
+            }
+            local dist = mag(dv)
+
+            -- normalize difference vector
+            dv.x = dv.x/dist
+            dv.y = dv.y/dist
+
+            -- accelerate the characters
+            l.vel.x = l.vel.x + dv.x * ke*l.q*mq/(dist*dist)
+            l.vel.y = l.vel.y + dv.y * ke*l.q*mq/(dist*dist)
+
+            -- move the characters
+            l.ul.x = l.ul.x + l.vel.x
+            l.ul.y = l.ul.y + l.vel.y
+            l.center.x = l.center.x + l.vel.x
+            l.center.y = l.center.y + l.vel.y
+
+            -- clamp the characters
+            if l.center.x < 0 then
+                l.center.x = 0
+                l.ul.x = -link.font:getWidth(l.char)/2
+            end if l.center.x > width then
+                l.center.x = width
+                l.ul.x = width-link.font:getWidth(l.char)/2
+            end
+
+            if l.center.y < 0 then
+                l.center.y = 0
+                l.ul.y = -link.font:getHeight(l.char)/2
+            end if l.center.y > height then
+                l.center.y = height
+                l.ul.y = height-link.font:getHeight(l.char)/2
+            end
+        end
+    end
 end
 
 function link.toggle_break()
-    if not link.broken then
+    if not link.is_broken then
         link.color = {0, 0, 0, 1}
-        link.broken = true
+        link.is_broken = true
     else
         link.color = {0, 0, 1, 1}
-        link.broken = false
+        link.is_broken = false
     end
+end
+
+function mag(vec)
+    return math.sqrt(vec.x*vec.x + vec.y+vec.y)
 end
